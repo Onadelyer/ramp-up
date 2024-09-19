41f78cd53ca710b04a6618f5b83f3f982ac27fc2
Onadelyer <tokarmacs63@gmail.com>
2024-09-19 21:17:16 +0300

    VPC module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "Name prefix for resources"
+  type        = string
+}
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
+variable "tags" {
+  description = "Map of tags to assign to resources"
+  type        = map(string)
+}

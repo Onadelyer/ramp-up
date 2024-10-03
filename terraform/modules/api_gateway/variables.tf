d63b6468228984da31e0386a59be49dd80805c06
Onadelyer <tokarmacs63@gmail.com>
2024-10-03 23:06:44 +0300

    Added API Gateway module

---
+++ terraform/modules/api_gateway/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type = string
+}
+
+variable "lambda_arn" {
+  type = string
+}
+
+variable "lambda_alias" {
+  type = string
+}
+
+variable "lambda_name" {
+  type = string
+}
